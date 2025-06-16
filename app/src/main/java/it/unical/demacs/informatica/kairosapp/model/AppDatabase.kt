9890c4b9908af3811b518ed9864f405febd967fc
Alessandro Sidero <75628365+Alessandro624@users.noreply.github.com>
2025-06-16 15:44:34 +0200

    Added local AppDatabase

--- app/src/main/java/it/unical/demacs/informatica/kairosapp/model/AppDatabase.kt
+++ app/src/main/java/it/unical/demacs/informatica/kairosapp/model/AppDatabase.kt
@@ -1,4 +1,31 @@
 package it.unical.demacs.informatica.kairosapp.model
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import io.swagger.client.models.EventDTO
+
+@Database(entities = [EventDTO::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun eventDao(): EventDAO
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var localInstance = instance
+                if (localInstance == null) {
+                    localInstance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "kairos-database"
+                    ).build()
+                    instance = localInstance
+                }
+                return localInstance
+            }
+        }
+    }
 }
